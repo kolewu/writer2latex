92ae340516ca4cce516a7a4e4c8d739195a547cc
Henrik Just <writer2latex@gmail.com>
2011-02-14 07:46:20 +0000

    Simplified w2l.bat
    git-svn-id: svn://svn.code.sf.net/p/writer2latex/code/trunk@87 f0f2a975-2e09-46c8-9428-3b39399b9f3c

--- source/java/writer2latex/api/ConverterFactory.java
+++ source/java/writer2latex/api/ConverterFactory.java
@@ -20,7 +20,7 @@
  *
  *  All Rights Reserved.
  * 
- *  Version 1.2 (2011-02-10)
+ *  Version 1.2 (2011-02-14)
  *
  */
  
@@ -33,7 +33,7 @@ public class ConverterFactory {
 
     // Version information
     private static final String VERSION = "1.1.6";
-    private static final String DATE = "2011-02-10";
+    private static final String DATE = "2011-02-14";
 	
     /** Return the Writer2LaTeX version in the form
      *  (major version).(minor version).(patch level)<br/>
